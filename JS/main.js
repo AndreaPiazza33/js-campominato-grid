08ec4f2baaa89a905d47b47de7376409a660a610
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-08-21 19:04:25 +0200

    fix: fixed function to create grid and cells

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,20 @@
 //elementi presi con id
 const myButton =document.getElementById('generator')
-
+const myGrid =document.getElementById('grid')
 
 //evento al click
 myButton.addEventListener('click', function () {
-
+    generatedGrid()
 })
+
+//funzione per creare la griglia
+function generatedGrid() {
+//ciclo per creare le celle
+    for (let i = 1; i <= 100; i++) {
+        const cell =document.createElement('div')
+        cell.classList.add('cell')
+        cell.innerText=(i)
+        myGrid.append(cell)
+    }
+}
 
